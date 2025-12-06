79f0eed39101e30bc8f8f657f50a0a1b4f94eb92
Uzmaa7 <uk9971khan@gmail.com>
2025-12-06 14:02:16 +0530

    Refactor repeated dragenter/dragleave logic by introducing addDragEventsOnColumn() and applying it to todo, progress, and done columns

--- index.js
+++ index.js
@@ -13,10 +13,27 @@ tasks.forEach(task =>{
     })
 })
 
-progress.addEventListener("dragenter", (e) => {
-    progress.classList.add("hover-over");
-})
 
-progress.addEventListener("dragleave", (e)=>{
-    progress.classList.remove("hover-over");
-})
+function addDragEventsOnColumn(column){
+    column.addEventListener("dragenter", (e)=>{
+        e.preventDeafault;
+        column.classList.add("hover-over");
+    })
+
+    column.addEventListener("dragleave", (e)=>{
+        e.preventDeafault;
+        column.classList.remove("hover-over");
+    })
+}
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
+
+
+// progress.addEventListener("dragenter", (e) => {
+//     progress.classList.add("hover-over");
+// })
+
+// progress.addEventListener("dragleave", (e)=>{
+//     progress.classList.remove("hover-over");
+// })
